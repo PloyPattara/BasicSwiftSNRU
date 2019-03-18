3af3bb5800f81137c40d2a8f2e0560f096ee717c
PloyNoii <pattharawadee.pe59@snru.ac.th>
2019-03-18 14:28:19 +0700

    Start Data Type

---
+++ DataTypeOfVariable.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+//String Type ตัวแปรที่เป็นอักษร
+
+var myName = "Nick" //นี้คือการประกาศ ที่อาศัยชนิดของ Value บอก Datatype
+var mySername: String = "Alexander" //นี้คือการประกาศแบบ กำหนด Datatype ตั้งแต่เกิด
+
+
---
+++ VariableAndContant.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//Explicit Variable
+//การประกาศตัวแปร
+
+var myName = "FangpongUNG"
+myName = "PloyNoii"
+
+//Explicit Constant
+//การประกาศค่าคงที่
+let myFriend = "Nick"
+//myFriend = "Jacob" Error Because myFriend is let
+
